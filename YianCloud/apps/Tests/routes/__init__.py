f83fa96b212ee1ac381962a839cd5c22d5a46bc9
SunYian <winksunyian@gmail.com>
2024-10-02 17:35:51 +0800

    modified:   YianCloud/apps/Tests/routes/__init__.py

--- YianCloud/apps/Tests/routes/__init__.py
+++ YianCloud/apps/Tests/routes/__init__.py
@@ -4,8 +4,3 @@ from .tests import test as tests_router
 router = APIRouter()
 
 router.include_router(tests_router, prefix="/tests")
-
-
-@router.get("/")
-async def root():
-    return {"message": "Welcome to YianCloud API"}
