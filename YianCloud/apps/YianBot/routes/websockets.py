26b96740c21a9cd7761437086e08963199122074
SunYian <winksunyian@gmail.com>
2024-10-21 17:36:40 +0800

    加上了一个简易测试

--- YianCloud/apps/YianBot/routes/websockets.py
+++ YianCloud/apps/YianBot/routes/websockets.py
@@ -5,7 +5,7 @@ from apps.YianBot.models import User
 router = APIRouter()
 
 
-@router.websocket()
+@router.websocket("/")
 async def websocket_endpoint(websocket: WebSocket):
     print("WebSocket连接")
     await websocket.accept()
@@ -16,3 +16,8 @@ async def websocket_endpoint(websocket: WebSocket):
             await websocket.send_text(response)
     except WebSocketDisconnect:
         print("WebSocket连接已断开")
+
+
+@router.get("/test")
+async def test():
+    return {"message": "这是websocket测试接口"}
