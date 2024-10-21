179b7776455bbafae9f9641b7d3dbfcc9554f039
SunYian <winksunyian@gmail.com>
2024-10-21 18:13:05 +0800

    移除多余的import

--- YianCloud/main.py
+++ YianCloud/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from tortoise import Tortoise
 from tortoise.contrib.fastapi import register_tortoise
