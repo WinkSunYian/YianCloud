3a0e44ce310f264e3cb3f9da98f87618311439aa
SunYian <winksunyian@gmail.com>
2024-10-22 14:48:50 +0800

    修复了server_status的response参数缺失

--- YianCloud/apps/YianBot/routes/server_status.py
+++ YianCloud/apps/YianBot/routes/server_status.py
@@ -1,17 +1,17 @@
 import sys
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from fastapi import __version__ as fastapi_version
 
 router = APIRouter()
 
 
 @router.get("/")
-def server_status(response, token: str | None = None):
+def server_status(response: Response, token: str | None = None):
     if token == "token":
         data = {
             "运行状态": "正常",
             "FastApi版本": fastapi_version,
-            "Python版本": sys.version,
+            "Python版本": sys.version_info,
         }
         return data
     else:
