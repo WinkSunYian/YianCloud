5147e55321bed92b2ed21fc396a30d12caf901bc
SunYian <winksunyian@gmail.com>
2024-10-23 12:38:04 +0800

    优化了无appkey请求时的报错

--- YianCloud/core/security.py
+++ YianCloud/core/security.py
@@ -1,10 +1,11 @@
 from fastapi import Header, HTTPException
 from configs.Config import AppKeyConfig
+from typing import Optional
 
 
-async def validate_app_key(app_key: str = Header(...)):
+async def validate_app_key(app_key: Optional[str] = Header(None)):
     if app_key is None:
-        raise HTTPException(status_code=403, detail="App Key is required")
+        raise HTTPException(status_code=403, detail="缺少AppKey")
     if app_key not in AppKeyConfig.APPKEYS:
-        raise HTTPException(status_code=403, detail="Invalid App Key")
+        raise HTTPException(status_code=403, detail="无效的AppKey")
     return app_key
