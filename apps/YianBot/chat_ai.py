9f3ac34c80cc6f665587ad88dc968b6611ee007e
SunYian <winksunyian@gmail.com>
2024-12-12 17:01:27 +0800

    修改了message的提交方式

--- apps/YianBot/chat_ai.py
+++ apps/YianBot/chat_ai.py
@@ -4,8 +4,10 @@ from common.error_code import ERROR_USER_NOT_FOUND
 from services.UserService import UserService
 from apps.YianBot.services.ChatService import ChatService
 from core.security import get_appkey
-
-
+from pydantic import BaseModel
+class ChatRequest(BaseModel):
+    message: str  # 通过请求体提交的消息字段
+    
 class chat_ai(ServiceRouter):
     def __init__(self):
         self.set_path("/users/{user_id}/chat-ai")
@@ -15,7 +17,7 @@ class chat_ai(ServiceRouter):
         self.setup_routes()
 
     async def post(
-        self, user_id: str, message: str, app_key: str = Depends(get_appkey)
+        self, user_id: str, chat_request: ChatRequest, app_key: str = Depends(get_appkey)
     ):
-        chat = await ChatService.get_gpt_response(user_id, message)
+        chat = await ChatService.get_gpt_response(user_id, chat_request.message)
         return self.res(data={"chat": chat})
