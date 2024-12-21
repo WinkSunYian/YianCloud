6424c5ff772f873f65aab27e20c2a5c761703380
SunYian <winksunyian@gmail.com>
2024-12-22 00:15:13 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   services/DialogueService.py

--- services/DialogueService.py
+++ services/DialogueService.py
@@ -3,9 +3,9 @@ from repositories.DialogueRepository import DialogueRepository
 
 class DialogueService:
     @staticmethod
-    async def get_by_user_id(user_id):
-        return await DialogueRepository.get_by_user_id(user_id)
+    async def get_by_user_id(user_id, limit: int):
+        return await DialogueRepository.get_by_user_id(user_id, limit)
 
     @staticmethod
-    async def create(user_id, message):
-        return await DialogueRepository.create(user_id, message)
+    async def create(user_id: int, role: str, content: str):
+        return await DialogueRepository.create(user_id, role, content)
