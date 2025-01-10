49d0c49f3405cabd4914b36d5052284ab7d20038
SunYian <winksunyian@gmail.com>
2025-01-10 15:20:32 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   common/exceptions/http_exceptions.py

--- common/exceptions/http_exceptions.py
+++ common/exceptions/http_exceptions.py
@@ -5,41 +5,49 @@ class NotFoundException(BaseHTTPException):
     def __init__(
         self,
         status_code: int = 404,
-        msg: str = "",
+        message: str = "",
         data: dict = None,
         detail: str = "资源不存在",
     ):
-        super().__init__(status_code=status_code, msg=msg, data=data, detail=detail)
+        super().__init__(
+            status_code=status_code, message=message, data=data, detail=detail
+        )
 
 
 class UserNotFoundException(BaseHTTPException):
     def __init__(
         self,
         status_code: int = 404,
-        msg: str = "",
+        message: str = "",
         data: dict = None,
         detail: str = "用户不存在",
     ):
-        super().__init__(status_code=status_code, msg=msg, data=data, detail=detail)
+        super().__init__(
+            status_code=status_code, message=message, data=data, detail=detail
+        )
 
 
 class TargetNotFoundException(BaseHTTPException):
     def __init__(
         self,
         status_code: int = 404,
-        msg: str = "",
+        message: str = "",
         data: dict = None,
         detail: str = "目标不存在",
     ):
-        super().__init__(status_code=status_code, msg=msg, data=data, detail=detail)
+        super().__init__(
+            status_code=status_code, message=message, data=data, detail=detail
+        )
 
 
 class TodaySignedException(BaseHTTPException):
     def __init__(
         self,
         status_code: int = 400,
-        msg: str = "",
+        message: str = "",
         data: dict = None,
         detail: str = "今日已签到",
     ):
-        super().__init__(status_code=status_code, msg=msg, data=data, detail=detail)
+        super().__init__(
+            status_code=status_code, message=message, data=data, detail=detail
+        )
