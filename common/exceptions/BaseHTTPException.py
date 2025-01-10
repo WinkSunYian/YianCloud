beda94bd55a080b9f8cc49744567827501d26bcf
SunYian <winksunyian@gmail.com>
2025-01-10 15:21:46 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   common/exceptions/BaseHTTPException.py

--- common/exceptions/BaseHTTPException.py
+++ common/exceptions/BaseHTTPException.py
@@ -3,19 +3,19 @@ from fastapi import HTTPException
 
 class BaseHTTPException(HTTPException):
     def __init__(
-        self, status_code: int, msg: str, data: dict = None, detail: str = None
+        self, status_code: int, message: str, data: dict = None, detail: str = None
     ):
         super().__init__(status_code=status_code, detail=detail)
 
         self.status_code = status_code
-        self.msg = msg
+        self.message = message
         self.data = data or {}
 
     def to_dict(self):
         """将异常信息转换为字典格式"""
         return {
             "status_code": self.status_code,
-            "msg": self.msg if self.msg else self.detail,
+            "message": self.message if self.message else self.detail,
             "detail": self.detail,
             "data": self.data,
         }
