365c97847daa8e68ea3b6ef910d88a9f7e271e4c
SunYian <winksunyian@gmail.com>
2025-02-12 13:08:41 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   apps/YianBot/users.py

--- apps/YianBot/users.py
+++ apps/YianBot/users.py
@@ -1,6 +1,5 @@
 from fastapi import Depends
 from utils.ServiceRouter import ServiceRouter
-from services.UserService import UserService
 from core.security import get_appkey
 from core.Container import Container
 from common.exceptions.http_exceptions import UserNotFoundException
@@ -8,15 +7,17 @@ from common.exceptions.http_exceptions import UserNotFoundException
 
 class UserRouter(ServiceRouter):
     def __init__(self):
+        self.container = Container()
+        self.user_service = self.container.get_user_service()
         self.set_path("/users/{user_id}")
         self.setup_routes()
 
     async def get(self, user_id, app_key=Depends(get_appkey)):
-        user = await UserService.get_user(user_id)
+        user = await self.user_service.get_user(user_id)
         if not user:
             raise UserNotFoundException()
         return self.res(data=user)
 
     async def post(self, user_id, app_key=Depends(get_appkey)):
-        user = await UserService.create(account=f"qq_{user_id}", qq=user_id)
+        user = await self.user_service.create(account=f"qq_{user_id}", qq=user_id)
         return self.res(status_code=201, data=user)
